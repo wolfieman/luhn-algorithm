8ca2adb54773d41176245cceda9b35a82921346a
wolfieman <wolfgang.sanyer@gmail.com>
2018-01-14 20:54:03 -0500

    Added Tests and Code for Null and Empty.

--- src/main/java/com/sanyer/tdd/luhn/Luhn.java
+++ src/main/java/com/sanyer/tdd/luhn/Luhn.java
@@ -1,20 +1,17 @@
 package com.sanyer.tdd.luhn;
 
 public class Luhn {
-	private String creditCard = "4012888888881881";
-	// private String creditCards[] = {"4012888888881881", "4012888888881881"};
+	private String creditCard = "";
 	private int sum = 0;
-	private int creditCardLength = creditCard.length();
-	private int parity = creditCardLength % 2;
+	private int creditCardLength = 0;
+	private int parity = 0;
 
 	public static void main(String[] args){
 		String creditCards[] = {"4012888888881881", "4012888888881882"};
-    	System.out.println("Hello World!");
+    	System.out.println("Luhn Algorithm Java Implementation:");
     	Luhn creditCard = new Luhn();
 
     	for (int i = 0; i < creditCards.length; i++) {
-    		// System.out.println(i);
-    		// System.out.println(creditCards[i]);
     		creditCard.checkLuhnCCValidity(creditCards[i]);
     	}
 	} 
@@ -31,8 +28,16 @@ public class Luhn {
     	return this.creditCardLength;
     }
 
+    public void setCreditCardLength (int length) {
+    	this.creditCardLength = length;
+    }
+
     public int getParity () {
     	return this.parity;
+    }
+
+    public void setParity (int parity) {
+    	this.parity = parity;
     }
 
     public int doubleIfParity (int index, int digit) {
@@ -49,7 +54,7 @@ public class Luhn {
     	return digit;
     }
 
-    public void printMessage(boolean isValid) {
+    public void printMessage(boolean isValid, String creditCard) {
     	if (isValid) {
     		System.out.println("Credit Card " + creditCard + " is valid.");
     	} else {
@@ -58,19 +63,24 @@ public class Luhn {
     }
 
     public boolean checkLuhnCCValidity (String creditCard) {
-    	
-    	for (int i=0; i < getCreditCardLength() ; i++) {
-    		int digit = Character.getNumericValue(creditCard.charAt(i));
+    	boolean isValid = false;
+    	if (!creditCard.isEmpty()) {
+	    	this.creditCardLength = creditCard.length();
+	    	this.parity = creditCardLength % 2;
 
-    		digit = adjustDigitIfGreaterThanNine(doubleIfParity(i, digit));
+	    	for (int i=0; i < getCreditCardLength() ; i++) {
+	    		int digit = Character.getNumericValue(creditCard.charAt(i));
 
-    		sum = sum + digit;
+	    		digit = adjustDigitIfGreaterThanNine(doubleIfParity(i, digit));
+
+	    		sum = sum + digit;
+	    	}
+
+	    	isValid = (sum % 10) == 0;
+	    	
+	    	printMessage(isValid, creditCard);
+	    	return isValid;
     	}
-
-    	boolean isValid = (sum % 10) == 0;
-    	
-    	printMessage(isValid);
-
     	return isValid;
     }
 }
--- src/test/java/com/sanyer/tdd/luhn/LuhnTest.java
+++ src/test/java/com/sanyer/tdd/luhn/LuhnTest.java
@@ -9,34 +9,51 @@ import com.sanyer.tdd.luhn.Luhn;
 
 public class LuhnTest {
     @Test
+    public void testCreditCardNull() {
+        Luhn creditCard = new Luhn();
+        Assert.assertTrue(creditCard.getCreditCard().isEmpty());
+    }
+
+    @Test
+    public void testCreditCardEmpty() {
+        Luhn creditCard = new Luhn();
+        creditCard.setCreditCard("");
+        Assert.assertTrue(creditCard.getCreditCard().isEmpty());
+    }
+
+    @Test
     public void testCreditCardNotNull() {
         Luhn creditCard = new Luhn();
+        creditCard.setCreditCard("4012888888881881");
         Assert.assertNotNull(Double.parseDouble(creditCard.getCreditCard()));
     }
 
     @Test
     public void testCreditCardLength() {
         Luhn creditCard = new Luhn();
+        creditCard.setCreditCardLength(16);
         assertEquals(16, creditCard.getCreditCardLength());
     }
 
     @Test
     public void testParity() {
         Luhn creditCard = new Luhn();
+        creditCard.setCreditCardLength(16);
+        creditCard.setParity(creditCard.getCreditCardLength() % 2);
         assertEquals(0, creditCard.getParity());
     }
 
     @Test
     public void testLuhnAlgorithmIsValid() {
         Luhn creditCard = new Luhn();
-        // assertTrue(creditCard.checkLuhnCCValidity(creditCard.getCreditCard()));  4012888888881881
-        assertTrue(creditCard.checkLuhnCCValidity("4012888888881881"));
+        creditCard.setCreditCard("4012888888881881");
+        assertTrue(creditCard.checkLuhnCCValidity(creditCard.getCreditCard()));
     }
 
     @Test
     public void testLuhnAlgorithmIsInvalid() {
         Luhn creditCard = new Luhn();
-        // assertTrue(creditCard.checkLuhnCCValidity(creditCard.getCreditCard()));  4012888888881881
-        assertFalse(creditCard.checkLuhnCCValidity("4012888888881882"));
+        creditCard.setCreditCard("4012888888881882");
+        assertFalse(creditCard.checkLuhnCCValidity(creditCard.getCreditCard()));
     }
 }
